f5bd0319cccfd411982e9845aeedc354da3dcf76
Dhanielr <dhanielr94@gmail.com>
2020-04-21 20:42:07 -0300

    Ajustes no Settings

--- madalena/madalena/settings.py
+++ madalena/madalena/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#!g+mvn#ogd-dz!$g+oexa=_vepn)$@)d*298ul61cluy365!1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
